d361c2e78f0f66c00dad0821bf42b97ae5ba4b60
Pongelupe <pongelupe1998@gmail.com>
2018-03-02 17:05:18 -0300

    adeus php, ola javolino

---
+++ src/main/java/com/br/coworkingSystem/CoworkingSystemApplication.java
@@ -0,0 +1,12 @@
+package com.br.coworkingSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoworkingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoworkingSystemApplication.class, args);
+	}
+}

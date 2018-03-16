262ec67e302c9b470e027e47893cf3b11c93271d
Ana <anasantos1103@gmail.com>
2018-03-16 20:01:33 -0300

    Criado rotas e menu

--- front-end/src/app.route.js
+++ front-end/src/app.route.js
@@ -3,19 +3,62 @@
 
 angular
     .module('coworkingApp').config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
-    $urlRouterProvider.otherwise("/home");
+    $urlRouterProvider.otherwise("/app");
 
     $stateProvider
-        .state("home", {
-            url: "/home",
+        .state("app", {
+            url: "/app",
             views: {
                 'app@': {
                     templateUrl: 'components/home/home.html',
                     controller: 'Main',
 
                 },
-                'contentApp@home': {
-                    template: "<p> Você está na HOME"
+                'menu@app':{
+                    templateUrl: 'components/menu/menu.html'
+                },
+                'contentApp@app': {
+                    template: "<p> Você está na página inicial"
+                }
+            }
+
+        })
+        .state("cliente", {
+            parent: "app",
+            url: "/cliente",
+            views: {
+                'contentApp@app': {
+                    template: "<p> Você está na aba cliente <p>"
+                }
+            }
+
+        })
+        .state("sala", {
+            parent: "app",
+            url: "/sala",
+            views: {
+                'contentApp@app': {
+                    template: "<p> Você está na aba sala <p>"
+                }
+            }
+
+        })
+        .state("faturamento", {
+            parent: "app",
+            url: "/faturamento",
+            views: {
+                'contentApp@app': {
+                    template: "<p> Você está na aba faturamento <p>"
+                }
+            }
+
+        })
+        .state("pagamento", {
+            parent: "app",
+            url: "/pagamento",
+            views: {
+                'contentApp@app': {
+                    template: "<p> Você está na aba pagamento <p>"
                 }
             }
 

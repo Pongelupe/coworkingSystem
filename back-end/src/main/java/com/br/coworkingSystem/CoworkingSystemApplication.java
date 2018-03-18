2988eb1b52339199103a829345ab9a6ebd016fb8
pongelupe <pongelupe1998@gmail.com>
2018-03-18 19:06:26 -0300

    separando as configuracoes de cors

--- back-end/src/main/java/com/br/coworkingSystem/CoworkingSystemApplication.java
+++ back-end/src/main/java/com/br/coworkingSystem/CoworkingSystemApplication.java
@@ -2,26 +2,12 @@ package com.br.coworkingSystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-@SuppressWarnings("deprecation")
+
 @SpringBootApplication
 public class CoworkingSystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CoworkingSystemApplication.class, args);
-	}
-	
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurerAdapter() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**");
-			}
-		};
 	}
 }
---
+++ back-end/src/main/java/com/br/coworkingSystem/configurations/CorsConfiguration.java
@@ -0,0 +1,18 @@
+package com.br.coworkingSystem.configurations;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@SuppressWarnings("deprecation")
+public class CorsConfiguration extends WebMvcConfigurerAdapter {
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**");
+	}
+
+}

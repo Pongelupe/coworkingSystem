23617c86512f244cf3579f27032b95ff26a994af
Ana <anasantos1103@gmail.com>
2018-03-18 00:59:41 -0300

    Ajustado tela do cliente

--- front-end/src/components/cliente/cliente.controller.js
+++ front-end/src/components/cliente/cliente.controller.js
@@ -4,29 +4,70 @@ angular.module('coworkingApp')
 
         vm.clientes = [
             {
+                tipoCliente: "fisico",
                 nome: "Ana Paula dos Santos",
+                dtNascimento: new Date("03/11/1996"),
                 cpfCnpj: "09487213669",
                 rg: "16587487",
-                endereco: "Rua estrela 195, Floramar, BH-MG"
+                cnh: "26256523",
+                telefone: "31975335875",
+                inscricaoMunicipal: "",
+                inscricaoEstadual: "",
+                endereco: "Rua estrela 195, Floramar, BH-MG",
+                email: "ana@teste.com.br"
             },
             {
-                nome: "Ana Paula dos Santos",
+                tipoCliente: "fisico",
+                nome: "Maria do Rosário",
+                dtNascimento: new Date("06/10/1973"),
                 cpfCnpj: "09487213669",
                 rg: "16587487",
-                endereco: "Rua estrela 195, Floramar, BH-MG"
+                cnh: "26256523",
+                telefone: "31975335875",
+                inscricaoMunicipal: "A6S5A88",
+                inscricaoEstadual: "AS21A54SA4",
+                endereco: "Rua estrela 195, Floramar, BH-MG",
+                email: "ana@teste.com.br"
             },
             {
-                nome: "Ana Paula dos Santos",
+                tipoCliente: "juridico",
+                nome: "Companhia Coworking System",
+                dtNascimento: new Date("01/01/1999"),
+                cpfCnpj: "10125698000147",
+                rg: "",
+                cnh: "",
+                telefone: "3134357858",
+                inscricaoMunicipal: "A6S5A88",
+                inscricaoEstadual: "AS21A54SA4",
+                endereco: "Rua estrela 659, Floramar, BH-MG",
+                email: "emp@teste.com.br"
+            },
+            {
+                tipoCliente: "fisico",
+                nome: "Gladston Monteiro de Carvalho",
+                dtNascimento: new Date("11/03/1996"),
+                cpfCnpj: "09547458448",
+                rg: "26598",
+                cnh: "95900",
+                telefone: "31999554762",
+                inscricaoMunicipal: "",
+                inscricaoEstadual: "",
+                endereco: "Rua estrela 195, Floramar, BH-MG",
+                email: "ga@teste.com.br"
+            },
+            {
+                tipoCliente: "fisico",
+                nome: "Breno Silva",
+                dtNascimento: new Date("11/03/1996"),
                 cpfCnpj: "09487213669",
                 rg: "16587487",
-                endereco: "Rua estrela 195, Floramar, BH-MG"
+                cnh: "26256523",
+                telefone: "31975335875",
+                inscricaoMunicipal: "A6S5A88",
+                inscricaoEstadual: "AS21A54SA4",
+                endereco: "Rua estrela 195, Floramar, BH-MG",
+                email: "ana@teste.com.br"
             },
-            {
-                nome: "Gladston Monteiro",
-                cpfCnpj: "09475412669",
-                rg: "18754252",
-                endereco: "Rua estrela 195, Floramar, BH-MG"
-            }
         ]
 
 
